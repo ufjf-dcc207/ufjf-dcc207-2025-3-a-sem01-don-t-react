54a6205266431a0d3133faf982d69b68cc4c3781
Victor Schiavo <victorschiavo343@gmail.com>
2025-11-03 17:50:49 -0300

    Implementa o código do component Column

--- quadro-kanban/src/components/Column.tsx
+++ quadro-kanban/src/components/Column.tsx
@@ -1,1 +1,35 @@
 // Esse arquivo irá conter as colunas
+
+import type { Card } from '../types';
+import CardItem from './CardItem';
+
+
+interface ColumnProps {
+    title: string;
+    cards: Card[];
+    onDropCard: (cardId: string, newColumn: string) => void;
+}
+
+const Column = ({title, cards, onDropCard}: ColumnProps) => {
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        const cardId = e.dataTransfer.getData("cardId");
+        onDropCard(cardId, title);
+    };
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    };
+    return (
+        <div className = "column" onDrop={handleDrop} onDragOver={handleDragOver}>
+            <h2>{title}</h2>
+            {cards.map((card) => (
+                <CardItem
+                key={card.id}
+                card={card}
+                onDragStart={(e, id) => e.dataTransfer.setData("cardId", id)}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Column;
