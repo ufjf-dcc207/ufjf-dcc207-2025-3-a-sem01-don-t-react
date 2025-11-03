4b73ef165ba26e63ad3529eec600045559ff92d3
Victor Schiavo <victorschiavo343@gmail.com>
2025-11-03 17:15:03 -0300

    Definindo a tipagem básica dos componentes Card e Column

--- quadro-kanban/src/types.ts
+++ quadro-kanban/src/types.ts
@@ -1,1 +1,10 @@
 // Esse arquivo irá centralizar as tipagens dos componentes do projeto
+
+export type Card = {
+    id: string;
+    title: string;
+};
+
+export type Columns = {
+    [key: string]: Card[];
+}
